0c18908ed0ac3c409c071b30ad51d2ff40b5d24f
Juraia Pinas <byenna21@gmail.com>
2022-01-09 20:45:17 +0100

    grab data from database and show into browser

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo 'connection error:' . mysqli_connect_error();
 }
 
 //write query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
 //make query & get result
 $result = mysqli_query($conn, $sql);
@@ -23,7 +23,7 @@ mysqli_free_result($result);
 //close connection
 mysqli_close($conn);
 
-print_r($pizzas);
+
 
 ?>
 
@@ -31,14 +31,32 @@ print_r($pizzas);
 <!DOCTYPE html>
 <html lang="en">
 
-    <?php
+    <?php include "templates/header.php" ?>;
 
-    include "templates/header.php";
+    <h4 class="center grey-text">Pizzas!</h4>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza) { ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                        </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a></div>
+                    </div>
+                </div>
+
+             <?php } ?>
+
+        </div>
+    </div>
+
+    <?php include "templates/footer.php" ?>;
 
     
-    include "templates/footer.php";
-
-    ?>
         
 
 </html>
